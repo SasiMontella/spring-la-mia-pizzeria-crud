ef0005305e0726491988d9726237f2539d456d6b
Sasi2294 <montella.salvatore@live.it>
2024-01-09 13:58:47 +0100

    Lavoro finito

--- src/main/java/org/learning/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/learning/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,13 +3,17 @@ package org.learning.springlamiapizzeriacrud.controller;
 import org.learning.springlamiapizzeriacrud.model.Pizza;
 import org.learning.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizza")
@@ -20,6 +24,17 @@ public class PizzaController {
     public String index(Model model){
         List<Pizza> pizzalist = pizzarepository.findAll();
         model.addAttribute("pizzalist", pizzalist);
-        return "";
+        return "pizzas/list";
+    }
+    @GetMapping("/show/{id}")
+    public String show(@PathVariable Integer id, Model model){
+        Optional<Pizza> result = pizzarepository.findById(id);
+        if (result.isPresent()){
+            Pizza pizza = result.get();
+            model.addAttribute("pizza", pizza);
+            return "pizzas/show";
+        } else {
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found");
+        }
     }
 }
